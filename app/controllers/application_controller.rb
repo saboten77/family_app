6871e1d6f888141f0be281e7495d0d4a2d1f57b7
saboten <yukidaaayo@gmail.com>
2026-07-05 11:23:13 +0900

    feat: ダッシュボードと下部ナビゲーションを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,7 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+  def after_sign_in_path_for(resource)
+  dashboard_path
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get "task_histories/index"
+  get "tasks/index"
+  get "dashboard/index"
   devise_for :users
   root "home#index"
-
+  get "dashboard", to: "dashboard#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   get "up" => "rails/health#show", as: :rails_health_check
+  get "tasks", to: "tasks#index"
+  get "task_histories", to: "task_histories#index"
 end
