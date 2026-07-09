1da42e984aa94a1528c200a6b23d8b159124390b
saboten <yukidaaayo@gmail.com>
2026-07-10 07:52:38 +0900

    chore: remove generated controller tests

--- test/controllers/contacts_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ContactsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get new_contact_path
-    assert_response :success
-  end
-end
