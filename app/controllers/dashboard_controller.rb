832b726513da8fe7ef056ef1fe213386059b56eb
saboten <yukidaaayo@gmail.com>
2026-07-15 08:59:56 +0900

    feat: 家庭エネルギー表示とステータスカードを実装

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,14 @@
 class DashboardController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @family = current_user.families.first
+
+    calculator = FamilyEnergyCalculator.new(@family)
+
+    @energy = calculator.call
+    @bonus = calculator.role_bonus
+
+    @background = "energy_high"
   end
 end
